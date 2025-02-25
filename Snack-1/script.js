218a98a4fd2c6024614ad893640a3c61b796885f
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-02-25 10:51:55 +0100

    ADD: Snack 1 solutions

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -3,3 +3,15 @@ function somma(a, b) {
 }
 
 somma(1, 2); // 3
+
+const sum = function(a, b) {
+    console.log(a + b);
+}
+
+sum(1, 4); // 5
+
+const add = (a, b) => {
+    console.log(a + b);
+}
+
+add(1, 6); // 7
